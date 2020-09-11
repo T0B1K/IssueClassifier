dbb1d5c81f5759de31bd9046424d0452e6a2a448
HendrikHenk <hendrik.henkelmann@gmail.com>
2020-09-11 20:53:48 +0200

    merge

--- classifier/loadData.py
+++ classifier/loadData.py
@@ -32,7 +32,7 @@ class DataPreprocessor:
             data=file.read()
         #we just take all the "text" from the JSON
         documents = list(map(lambda entry: entry["text"], json.loads(data)))
-        return np.array(documents)
+        return np.array(documents[:10])
 
     # load data from label categories
     def loadDataFromClasses(self, consoleOutput=True):
