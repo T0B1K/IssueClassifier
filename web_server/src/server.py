5feb420bd27b024631a63b4721247b00b0e4f3c5
Aly Mohamed <st159580@stud.uni-stuttgart.de>
2020-09-23 13:17:46 +0200

    Prepare first set-up of MQTT on Flask
    This commit adds an MQTT client into the flask application. Currently, this is the bare-bones required to connect to an MQTT broker (such as Eclipse Mosquitto as a Docker container) and subscribe to a topic.

--- web_server/src/server.py
+++ web_server/src/server.py
@@ -1,46 +1,36 @@
-import json
-
-from flask import Flask, request
-from flask_restx import Api, Resource, fields
-from http import HTTPStatus
+from flask import Flask
+from flask_mqtt import Mqtt
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-from .models import Issue
 from .schemas import IssueSchema
-from .server_helper import process_issues
 
 # Flask setup
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
-api = Api(app, version='1.0', title='Issue Classifier Microsrevice',
-          description='HTTP endpoint for the issue classifier microservice')
 
-# Setting up Swagger UI
-classification = api.namespace(
-    'classification', description='Classification operations')
-issue_model = api.model(
-    'Issue', {
-        'body': fields.String(required=True, description='The issue body'),
-    }
-)
+# MQTTT Setup
+app.config['MQTT_BROKER_URL'] = 'localhost'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
+mqtt = Mqtt(app)
 
-# Initialising a Marshmallow scheme for input validation
+# Marshmallow Setup
 issue_schema = IssueSchema(many=True)
 
 
-@classification.route('/classify')
-class IssueResource(Resource):
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    mqtt.subscribe('classification/classify')
 
-    @classification.expect(issue_model)
-    @classification.marshal_list_with(issue_model)
-    def post(self):
-        request_issues = request.get_json()
 
-        if request_issues is None:
-            return ({"Error": "Either the Content-Type header wasn't specified as application/json or the request body is empty."}, HTTPStatus.BAD_REQUEST.value)
-
-        error_messages = issue_schema.validate(request_issues)
-        if not error_messages:
-            return (error_messages, HTTPStatus.BAD_REQUEST.value)
-
-        return process_issues(request_issues)
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topics=message.topic,
+        payload=message.payload.decode()
+    )
+    print(data)
