3f0b94bb8838b75d31de044fb89db0570feaca43
schurpl <st155930@stud.uni-stuttgart.degit config --global user.email st155930@stud.uni-stuttgart.de>
2020-09-30 16:24:37 +0200

    Add loadFIle for Classifers and Vectorizer

---
+++ classifier/loadClassifier.py
@@ -0,0 +1,21 @@
+import json
+import joblib
+
+rawConfig = open ('classifier/loadConfig.json','r') 
+loadConfig = json.load(rawConfig)
+classifier_locations = loadConfig['classifier_locations']
+rootFolder = loadConfig['classifierFolder']
+
+
+def getClassifier (categorie):
+    classifierPath = classifier_locations['{}_{}'.format(categorie[0],categorie[1])]
+    path = "{}/{}".format(rootFolder,classifierPath)
+    classifier = joblib.load(path)
+    return classifier
+
+def getVectorizer ():
+    vecpath = 'vectorizer.vz'
+    vectorizer = joblib.load(vecpath)
+    return vectorizer
+
+    
