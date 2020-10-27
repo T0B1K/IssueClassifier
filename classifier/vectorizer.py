a032d8e6e4efd3bd4a30bd4ce1637ac16b2bd474
Tobias Kässmann <st155019@stud.uni-stuttgart.de>
2020-10-27 17:08:36 +0100

    vectorizer docstring refactoring, documentation

--- classifier/vectorizer.py
+++ classifier/vectorizer.py
@@ -8,66 +8,81 @@ import numpy
 import file_manipulation
 import load_classifier
 
-"""
-This class is used for creating / loading an Vectorizer, which is used to create feature vectors out of the documents
-"""
-
 
 class Vectorizer(file_manipulation.FileManipulation):
+    """This class is used for creating / loading an Vectorizer, which is used to create feature vectors out of the documents
+
+    Args:
+        file_manipulation (FileManipulation): This class is used to access data from documents as well as to save them later on
+    """
 
     def __init__(self, ngram: tuple = (1, 2), stripAccents=None, stopWords=None):
+        """This is the constructor for the Vectorrizer class
+
+        Args:
+            ngram (tuple, optional): how many neighbor words should be taken into consideration when creating an featurevector. Defaults to (1, 2).
+            stripAccents (str, optional): Thich accents should be stripped 'ASCII', 'UNICODE'. Defaults to None.
+            stopWords (list, optional): Which stopwords should be removed i.e. {'english'}. Defaults to None.
         """
-        This is the constructor for the Vectorrizer class
-        Input:  loadVec (optional)      true is default - should the vectorrizer be loaded
-                saveVec (optional)      false is default- should the vectorrizer be saved
-                ngram   (optional)      (1,2) is default- means the word itself and the neighbor
-                stripAccents (optional) should the accents be stripped 
-                stopWords (optional)    should stopwords be removed
-        """
+        
         super().__init__()
         self.Vecotrizer = self.prepareVectorizer(
             stripAccents, ngram, stopWords)
 
     def stemmer(self, text: list) -> list:
+        """This method is used for stemming tokens. I.e. cat should identify such strings as cats, catlike, and catty
+
+        Args:
+            text (list): a list of texts / documents
+
+        Returns:
+            list: the same document list, but the tokens are now stemmed
         """
-        Description: This method is used for stemming tokens. I.e. cat should identify such strings as cats, catlike, and catty
-        Input: List[String] of documents
-        Output: List[String] of documents with stemmed tokens
-        """
+        
         return [PorterStemmer().stem(token) for token in text]
 
     def lemmatizer(self, text: list) -> list:
+        """!Attention - currently not in use - just used for performance testing! TODO remove in final version 
+        This method can be used for lemmatizing tokens. I.e.  "better" is mapped to "good" or "walking" to "walk"
+
+         Args:
+            text (list): a list of texts / documents
+
+        Returns:
+            list: the same document list, but the tokens are now stemmed
         """
-        !Attention - currently not in use - just used for performance testing! TODO remove in final version 
-        Description: This method is used for lemmatizing tokens. I.e.  "better" is mapped to "good" or "walking" to "walk"
-        Input: List[String] of documents
-        Output: List[String] of lemmatized documents
-        """
+        
         return [WordNetLemmatizer().lemmatize(token) for token in text]
 
     def createFeatureVectors(self, X_train_documents: numpy.ndarray, X_test_documents: numpy.ndarray) -> tuple:
+        """This method is used for the vectorisation of the training and testing documents (creating tf-idf, ngram vectors out of the data)
+        So it is basically creating a vector (number vector) out of the trainingsdata (string vector) and removes the as the stopwords, emojis, ... if selected
+
+        Args:
+            X_train_documents (numpy.ndarray): List of unvectorized text documents
+            X_test_documents (numpy.ndarray): List of unvectorized text documents
+
+        Returns:
+            tuple:  X_train_vectorized (numpy.ndarray): List of tfidf weighted / vectorized test documents
+                    X_test_vectorized  (numpy.ndarray): List of tfidf weighted / vectorized train documents
         """
-        Description: This method is used for the vectorisation of the training and testing documents (creating tf-idf, ngram vectors out of the data)
-        Input:  X_train_documents:  List[String] of unvectorized text documents
-                X_test_documents:   List[String] of unvectorized text documents
-        Output: X_train_vectorized: List[String] of tfidf vectorized text documents
-                X_test_vectorized:  List[String] of tfidf vectorized text documents
-        """
-        # the vectorizer is creating a vector out of the trainingsdata (bow) as well as removing the stopwords and emojis (non ascii) etc.
-        X_train_vectorized: scipy.sparse.csr.csr_matrix = self.Vecotrizer.transform(
-            X_train_documents)
-        X_test_vectorized: scipy.sparse.csr.csr_matrix = self.Vecotrizer.transform(
-            X_test_documents)  # vectorisation
+
+        X_train_vectorized: scipy.sparse.csr.csr_matrix = self.Vecotrizer.transform(X_train_documents)
+        X_test_vectorized: scipy.sparse.csr.csr_matrix = self.Vecotrizer.transform(X_test_documents)
         return X_train_vectorized, X_test_vectorized
 
-    # TODO refactor
     def prepareVectorizer(self, stripAccents, ngram, stopWords) -> TfidfVectorizer:
+        """This method loads a vectorrizer from an .vz file if this file it exists. Otherwise it will create a new vectorrizer
+
+        Args:
+            stripAccents (str, optional): Thich accents should be stripped 'ASCII', 'UNICODE'. Defaults to None.
+            ngram (tuple, optional): how many neighbor words should be taken into consideration when creating an featurevector. Defaults to (1, 2).
+            stopWords (list, optional): Which stopwords should be removed i.e. {'english'}. Defaults to None.
+
+        Returns:
+            TfidfVectorizer: An loaded or newly created TfidfVectorizer object
         """
-        Description: This method is used to load the vectorrizer from an .vz file, if it exists, or to create a vectorrizer
-        Input:  loadVec: Boolean    whether the vectorrizer should be loaded or not
-                saveVec: Boolean    whether the vectorrizer should be saved afterwards or not
-        Output: an loaded or newly created TfidfVectorizer object
-        """
+        
         loadVec:bool = file_manipulation.FileManipulation.values["vectorrizer"]["loadVectorizer"]
         if loadVec == True:
             return self.createNewVectorizer(stripAccents, ngram, stopWords)
@@ -76,13 +91,22 @@ class Vectorizer(file_manipulation.FileManipulation):
         except:
             return self.createNewVectorizer(stripAccents, ngram, stopWords)
 
-    # TODO pls add comment
     def createNewVectorizer(self, stripAccents, ngram: tuple, stopWords) -> TfidfVectorizer:
+        """This method creates a new tfidf vectorrizer and returns it
+
+        Args:
+            stripAccents (str, optional): Thich accents should be stripped 'ASCII', 'UNICODE'. Defaults to None.
+            ngram (tuple, optional): how many neighbor words should be taken into consideration when creating an featurevector. Defaults to (1, 2).
+            stopWords (list, optional): Which stopwords should be removed i.e. {'english'}. Defaults to None.
+
+        Returns:
+            TfidfVectorizer: The newly created tfidf vectorrizer object
+        """
         saveVec:bool = file_manipulation.FileManipulation.values["vectorrizer"]["saveVectorrizer"]
 
         train_Data:numpy.ndarray = self.getSplitedDocs(
             file_manipulation.FileManipulation.values["trainingConstants"]["sampleSize"])
-        Vecotrizer = TfidfVectorizer(tokenizer=None,
+        Vecotrizer: TfidfVectorizer = TfidfVectorizer(tokenizer=None,
                                      strip_accents=stripAccents, lowercase=None, ngram_range=ngram,
                                      stop_words=stopWords,
                                      min_df=2)
@@ -93,11 +117,16 @@ class Vectorizer(file_manipulation.FileManipulation):
         return Vecotrizer
 
     def getSplitedDocs(self, sampleSize:int) -> numpy.ndarray:
-        """
-        Description: This method is used for getting an equal amount of documents from each label class
-        Input:  samplesize :int     how many documents should be returned
-        Output: List[String]        the documents from the different label classes
-        """
+        """This method is used for getting an equal amount of documents from each label class
+        by calculating the right amount and randomly selecting certain documents from each label class provided in the config
+
+        Args:
+            sampleSize (int): how many documents should be returned
+
+        Returns:
+            numpy.ndarray: a list of {sampleSize} documents, with an equal amount of documents in each label class provided in the config
+        """   
+        
         labelClasses:list = file_manipulation.FileManipulation.values["labelClasses"]
         length:int = len(labelClasses)
         docCount:float = round(sampleSize / length)
