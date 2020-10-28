d0fa5197cc99e1d8eda8db5fc52ac758fa9ae8f5
HendrikHenk <hendrik.henkelmann@gmail.com>
2020-10-28 19:49:34 +0100

    refactored the configuration code

--- classifier/configuration.py
+++ classifier/configuration.py
@@ -7,20 +7,12 @@ class Configuration():
     def __init__(self):
         """Description: Constructor for Configuration""" 
 
-    def resolvePath(self, config, path):
-        """Description: Iterates recursively through the configuration tree to a value.
-        Input: Array of JSON keys and the configuration JSON.
-        Output: String containing the desired configuration value."""
-        if len(path) == 0:
-            return config
-        else:
-            config = config[path[0]]
-            path.pop(0) 
-            return self.resolvePath(config, path)
-
     def getValueFromConfig(self, configPath):
-        """Description: Splits the input string into an array and passes it to the resolvePath() function.
+        """Description: Splits the input string into an array of key values and iterates through the configuration JSON.
         Input: String containing the keys of the desired configuration value.
         Output: Returns the desired configuration value as a string."""
         path = configPath.split()
-        return self.resolvePath(self.configValues, path)
+        config = self.configValues
+        for key in path:
+            config = config[key]
+        return config
