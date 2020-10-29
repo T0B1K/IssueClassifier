d0910380278cff26edf087e2e8bdb839b3499517
Tobias Kässmann <st155019@stud.uni-stuttgart.de>
2020-10-29 12:17:13 +0100

    refactoring of the comments and data types of the file file_manipulation

--- classifier/file_manipulation.py
+++ classifier/file_manipulation.py
@@ -7,34 +7,40 @@ import configuration
 
 config = configuration.Configuration()
 
-
-""" Class provies methods for loading and writthing files with additional parameters """
-
-
 class FileManipulation:
+    """ Class provies methods for loading and writthing files with additional parameters """
     
     def __init__(self):
-        """Description: Constructor for FileManipulation
-        Input:  outputFolder optional path parameter 
-        Output: Return FileManipulation object"""
+        """Constructor for FileManipulation
+        """
 
     def getRandomDocs(self, label:str, elementcount:int) -> numpy.ndarray:
-        """Description: get random Documents a class
-        Input:  label of the document class
-                elementcount amount of documents to load 
-        Output: Return List[String] of documents"""
+        """This method is used for getting random documents from a labelclass
+
+        Args:
+            label (str): The label i.e. "bug"
+            elementcount (int): How many documents should be randomly choosen from the corresponding file
+
+        Returns:
+            numpy.ndarray: The randomly chosen documents of the specific file
+        """
+        
         path:str = "{}/{}.json".format(config.getValueFromConfig("issueFolder"), label)
         data:numpy.ndarray = self.openFile(path, elementcount)
         perm:numpy.ndarray = numpy.random.permutation(data.shape[0])
         return data[perm]
 
     def openFile(self, filename:str, elementcount:int=config.getValueFromConfig("trainingConstants elementCount")) -> numpy.ndarray:
+        """Method loads a file
+
+        Args:
+            filename (str): The name of the file
+            elementcount (int, optional): The amount of elements of the loaded documents. Defaults to config.getValueFromConfig("trainingConstants elementCount").
+
+        Returns:
+            numpy.ndarray: list of loaded documents
         """
-        Description: Method loads file 
-        Input:  filename name of the file
-                elementcount amount of documents to load optional
-        Output: Return List[String] of documents
-        """
+        
         with open(filename, "r") as file:
             data = file.read()
         # we just take all the "text" from the JSON
@@ -43,24 +49,26 @@ class FileManipulation:
         return numpy.array(documents)
 
     def saveAntmapToFile(self, filename:str, data:str):
+        """This method is used to save the antmap to a file
+
+        Args:
+            filename (str): The name of the file, where to store the antmap
+            data (str): The data of the antmap (the antmap itself)
         """
-        Description: Method to save Antmap to file
-        Input:  filename name of the file
-                data to save
-        Output: Return nothing
-        """
+        
         path:str = "{}/{}".format(config.getValueFromConfig("outputFolder"), filename)
         logging.info(">\tsaving antmap in {}".format(path))
         with open(path, "w", encoding='utf-8', errors='ignore') as f:
             f.write(data)
 
     def saveWrongClassifiedToFile(self, filename:str, data:str):
+        """This method saves the wrong classified issues to a file
+
+        Args:
+            filename (str): The filename of the file
+            data (str): The wrong classified issues
         """
-        Description: Method to save wrong Classified 
-        Input:  filename name of the file
-                data to save
-        Output: Nothing
-        """
+        
         path:str = filename  # self.outputFolder+"/"+filename
         logging.info(">\tsaving Wrong Classified Texts in {}".format(path))
         with open(path, "w", encoding='utf-8', errors='ignore') as f:
