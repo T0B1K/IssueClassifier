37a4e3f5a64d61acc4c7adeb3888211804fd5e15
Tobias Kässmann <st155019@stud.uni-stuttgart.de>
2020-10-29 11:17:26 +0100

    changed config doku

--- classifier/configuration.py
+++ classifier/configuration.py
@@ -5,12 +5,18 @@ class Configuration():
     configValues = json.loads(open("load_config.json").read())
 
     def __init__(self):
-        """Description: Constructor for Configuration""" 
+        """Constructor for Configuration
+        """ 
 
-    def getValueFromConfig(self, configPath):
-        """Description: Splits the input string into an array of key values and iterates through the configuration JSON.
-        Input: String containing the keys of the desired configuration value.
-        Output: Returns the desired configuration value as a string."""
+    def getValueFromConfig(self, configPath: str):
+        """Splits the input string into an array of key values and iterates through the configuration JSON.
+
+        Args:
+            configPath (str): String containing the keys of the desired configuration value.
+
+        Returns:
+            [type]: Returns the desired configuration value as a string or array.
+        """
         path = configPath.split()
         config = self.configValues
         for key in path:
