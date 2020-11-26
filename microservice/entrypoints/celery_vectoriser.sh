01a52da07747f67bbec01a2ae8d4170aec5a2950
Aly Mohamed <st159580@stud.uni-stuttgart.de>
2020-11-26 19:49:55 +0100

    Change vectoriser execution pool to solo

--- microservice/entrypoints/celery_vectoriser.sh
+++ microservice/entrypoints/celery_vectoriser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 15
-celery -A microservice.classifier_celery.celery worker -l INFO -P prefork -Q vectorise_queue -n vectoriser@%n
+celery -A microservice.classifier_celery.celery worker -l INFO -P solo -Q vectorise_queue -n vectoriser@%n
